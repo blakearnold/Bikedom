8fc0f33cd8b3717165008e574b7b381f88b1fc61
Blake Arnold <bablake@gmail.com>
2010-11-11 18:35:26 -0800

    adding initial home page

---
+++ bikedomHome.php
@@ -0,0 +1,34 @@
+<html>
+<head>
+	<title>Bikedom, a new way to share</title>
+</head>
+<div class="header">
+<h1>BIKEDOM</h1>
+<div class="subhead">a new way to share</div>
+<div class = "links">
+<ul>
+<li><a href="faq.php">FAQ</a></li>
+<li><a href="account.php">My Account</a></li>
+</ul>
+</div>
+</div>
+
+<div class="body">
+<div class="rent"><a href="rent.php">Rent A Bike</a> </div>
+<div class="list"><a href="list.php">List A Bike</a><div>
+
+
+<div class="desciption">
+<p> Ever needed or wanted to use a bike but don't want own one? Do you own a bike but feel that it's under used? Rent and List bikes on Bikedom!</p>
+
+</p>
+</div>
+</div>
+<div class="footer">
+<ul>
+<li><a href="sitemap.php">Sitemap</a></li>
+<li><a href="faq.php">FAQ</a></li>
+</ul>
+
+<div>
+</head>
