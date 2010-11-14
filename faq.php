ae159321e13ca455328479cde492c15355274f96
Blake A Arnold <baa2121@rome.clic.cs.columbia.edu>
2010-11-14 02:03:27 -0500

    adding faq

---
+++ faq.php
@@ -0,0 +1,38 @@
+<?php 
+include 'layout.php';
+print_head_start();
+?>
+</head>
+<body>
+<?php
+print_header();
+?>
+<div class="body">
+
+
+<div class="faq">
+<H2>For Renters</h2>
+<p>How do I pick up a bike?</p>
+By reserving a bicycle you will receive the time, location and lock combination of the bicycle. You will have an alloted time to use the bike.
+<p>What if I do not return the bicycle in time?</p>
+You will be charged a premium of 1.5 of the standard rate.
+<p>What if I damage the bike?</P>
+You are responsible for all damages to the bicycle, you can opt-in to purchase insurance for the rental.
+
+<h2>For Owners</h2>
+<p>Why Rent your bike out?</p>
+Our site allows you to have another source of income. Besides the added money, our company is teaming up with local bike shops to give you free reapirs after every 10 rentals.
+<p>What if my bike is damaged?</p>
+If your bike is damaged in any way, fill out the damage form that corresponds to your bike rental. The Renter has 10 days to dispute the claim before you are reimbursed.
+<p>What if my bike is stolen</p>
+Our company has taken out a $10,000 insurance policy to ensure all stolen and lost bicycles are fully payed for.
+
+</p>
+</div>
+</div>
+
+<?php print_footer() ?>
+
+</body>
+
+</html>
