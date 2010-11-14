5db52ec493f888bbb5fd209f48f1ba091c91131d
Blake A Arnold <baa2121@rome.clic.cs.columbia.edu>
2010-11-13 23:01:11 -0500

    added maps

--- rent.php
+++ rent.php
@@ -1,16 +1,27 @@
 <?php 
-include 'layout.php' 
-print_head();
+include 'layout.php'; 
+print_head_start();
 ?>
+		<meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
+		<style type="text/css">
+		html { height: 100% }
+	  	body { height: 100%; margin: 0px; padding: 0px }
+	    #map_canvas { height: 100% }
+	  	</style>
+		<script type="text/javascript"
+		      src="http://maps.google.com/maps/api/js?sensor=false">
+		 </script>
+		 <script type="text/javascript" src="js/maps.js">
+	  	</script>
+</head>  
 <body>
 
-<?php print_header() ?>
-<div class="body">
+<?php print_header(); ?>
+<body onload="load()">
+<div id="map_canvas" style="width:100%; height:100%"></div>
+</body>
 
-
-</div>
-
-<?php print_footer() ?>
+<?php print_footer(); ?>
 
 </body>
 
