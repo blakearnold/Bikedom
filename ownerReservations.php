8e195f5b58d036cc876b22e63a04c30b6e681253
Anish Bramhandkar <ab2929@ottawa.clic.cs.columbia.edu>
2010-11-14 19:26:46 -0500

    ownerReservations.php

---
+++ ownerReservations.php
@@ -0,0 +1,31 @@
+<?php 
+include 'layout.php';
+include 'helper.php';
+forceLogin();
+print_head_start();
+?>
+</head>
+<body>
+<?php
+print_header();
+?>
+<div class="dashboard">
+<h3>Your reservations:</h3>
+<div class="accountform">
+<table width=700 align="center">
+<col width=10%>
+<col width=25%>
+<col width=40%>
+<col width=25%>
+<tr><td><h4>Bike ID</hr><td><h4>Reserved from</h4><td><h4>Time period</h4><td><h4>Cancel?</h4>
+<tr><td>2<td>Paul_Blaer<td class="pastreserve">2010-01-01 16:00 to<br />2010-01-01 21:00<td>
+<tr><td>3<td>Junfeng_Yang<td class="futurereserve">2010-11-27 16:00 to<br />2010-11-27 21:00<td><div class="buttons"><a href="cancel.php" class='button2'>Cancel</a></div>
+</table>
+</div>
+</div>
+
+<?php print_footer() ?>
+
+</body>
+
+</html>
