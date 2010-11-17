b163e480dbdbf48623a3791d0c17d8a253a1f8a1
Blake A Arnold <baa2121@canberra.clic.cs.columbia.edu>
2010-11-17 15:23:56 -0500

    modified rent to have date field

--- js/maps.js
+++ js/maps.js
@@ -1,55 +1,101 @@
 var lastInfo;
+var markers = new Array();
+var markerHash = new Array();
 function load() {
-    
+
 	var latlng = new google.maps.LatLng(40.76273, -73.985023);
 	var myOptions = {
-	    zoom: 12,
-	    center: latlng,
-	    mapTypeId: google.maps.MapTypeId.ROADMAP
-	 };
-	 var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+zoom: 12,
+	  center: latlng,
+	  mapTypeId: google.maps.MapTypeId.ROADMAP
+	};
+	map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+	downloadAndLoad();
+}
 
- 
-$xmlLoc = "dblib/genxml.php";
-if (window.XMLHttpRequest)
-	  {// code for IE7+, Firefox, Chrome, Opera, Safari
-		    xmlhttp=new XMLHttpRequest();
-			  }
-else
-  {// code for IE6, IE5
-	    xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-		  }
+function downloadAndLoad(params){
+	if(params != ""){
+		$xmlLoc = "dblib/genxml.php";
+	} else {
+		$xmlLoc = "dblib/genxml.php?" + params;
+	}
+	if (window.XMLHttpRequest)
+	{// code for IE7+, Firefox, Chrome, Opera, Safari
+		xmlhttp=new XMLHttpRequest();
+	}
+	else
+	{// code for IE6, IE5
+		xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+	}
 
-xmlhttp.open("GET",$xmlLoc,false);
-xmlhttp.send();
-xmlDoc=xmlhttp.responseXML;
+	xmlhttp.open("GET",$xmlLoc,false);
+	xmlhttp.send();
+	xmlDoc=xmlhttp.responseXML;
 	if(xmlDoc){
 		var markers = xmlDoc.documentElement.getElementsByTagName("bike");
 		for (var i = 0; i < markers.length; i++) {
-		    var rate = markers[i].getAttribute("rate");
-		    var address = markers[i].getAttribute("address");
-		    var bserial = markers[i].getAttribute("bserial");
-		    var type = markers[i].getAttribute("type");
-		    var point = new google.maps.LatLng(parseFloat(markers[i].getAttribute("lat")),
-			parseFloat(markers[i].getAttribute("lng")));
+			var rate = markers[i].getAttribute("rate");
+			var address = markers[i].getAttribute("address");
+			var bserial = markers[i].getAttribute("bserial");
+			var type = markers[i].getAttribute("type");
+			var point = new google.maps.LatLng(parseFloat(markers[i].getAttribute("lat")),
+					parseFloat(markers[i].getAttribute("lng")));
 			createMarker(point, map, rate, address, type, bserial);
-		
-	    }
+
+		}
 	}
-    
+
+}
+
+function updateMap(){
+
+	$startDate = document.getElementById("stardate");
+	$startTime = document.getElementById("starttimehour");
+	$startTime= $startTime + ":" + document.getElementById("starttimeminute").value;
+	$endDate = document.getElementById("enddate");
+
+	$endTime= document.getElementById("endtimehour");
+
+	$endTime= $endTime + ":" + document.getElementById("endtimeminute").value;
+	$endTime = document.getElementById("enddate");
+	$query = "startDate=" + $startDate + "&" +
+			"startTime=" + $startTime + "&" +
+			"endDate=" + $endDate + "&" +
+			"endTime=" + $endTime;
+	removeMarkers();	
+	alert($query);
+//	downloadAndLoad(query);
+
+}
+
+function removeMarkers(){
+	for(marker in markers){
+		alert(typeof markers.getPosition);
+		if(typeof marker.setVisible == 'function')
+			marker.setVisible(false);
+	}
+	markers = new Array();
 }
 
 function createMarker(point, map, rate, address, type, bserial) {
-    var marker = new google.maps.Marker({
-		position: point,
-		map: map,
-	    title: type});
-	    var html = "<b>" + address + ":</b> <br/> Type: " + type + "<br/> Rate: " + rate + "<br> <a href='reservation.php?bikeId=" + bserial + "'>Reserve this Bike!</a>";
-    	google.maps.event.addListener(marker, 'click', function() {
+	if(marker = markerHash[bserial]){
+		marker.setVisible(true);
+		markers.push(marker);
+	} else {
+		var marker = new google.maps.Marker({
+position: point,
+map: map,
+title: type});
+		markers.push(marker);
+		markerHash[bserial] = marker;
+		var html = "<b>" + address + ":</b> <br/> Type: " + type + "<br/> Rate: " + rate + "<br> <a href='reservation.php?bikeId=" + bserial + "'>Reserve this Bike!</a>";
+		google.maps.event.addListener(marker, 'click', function() {
 			if(lastInfo){
-				lastInfo.close();
+			lastInfo.close();
 			}
 			lastInfo = new google.maps.InfoWindow({content: html});
 			lastInfo.open(map,marker);
-	});
+			});
+	}
 }
+
--- rent.php
+++ rent.php
@@ -2,18 +2,43 @@
 include 'layout.php'; 
 print_head_start();
 ?>
-		<meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
-		<script type="text/javascript"
-		      src="http://maps.google.com/maps/api/js?sensor=false">
-		 </script>
-		 <script type="text/javascript" src="js/maps.js">
-	  	</script>
-</head>  
-<body>
-
-<?php print_header(); ?>
+	<meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
+	<script type="text/javascript"
+	src="http://maps.google.com/maps/api/js?sensor=false">
+	</script>
+	<script type="text/javascript" src="js/maps.js">
+	</script>
+	<script src="js/date-functions.js" type="text/javascript"></script>
+	<script src="js/datechooser.js" type="text/javascript"></script>
+	<link rel="stylesheet" type="text/css" href="css/datechooser.css">
+	<link rel="stylesheet" type="text/css" href="css/map.css">
+	<!--[if lte IE 6.5]>
+	<link rel="stylesheet" type="text/css" href="css/select-free.css"/>
+	<![endif]-->
+	</head>  
 <body onload="load()">
-<div id="map_canvas" style="width:100%; height:100%"></div>
+<?php print_header(); ?>
+<div id="sidebar">
+
+<div class="mapform_start">
+	Start Date: <input id="startdate" size="10" maxlength="10" name="startdate" type="text"><img src="calendar.gif" onclick="showChooser(this, 'startdate', 'chooserSpan3', 2010, 2011, Date.patterns.ShortDatePattern, false);"> 
+
+<div id="chooserSpan3" class="dateChooser select-free" style="display: none; visibility: hidden; width: 160px;"></div>
+</div>
+<div id="starttime">Start time<td><select name="starttimehour" id="starttimehour"><?php print_timeselecthour() ?>:<select name="starttimeminute" id="starttimeminute"><?php print_timeselectminute() ?></div>
+<div class="mapform_end">
+	End Date: <input id="enddate" size="10" maxlength="10" name="enddate" id="enddate" type="text"><img src="calendar.gif" onclick="showChooser(this, 'enddate', 'chooserSpan2', 2010, 2011, Date.patterns.ShortDatePattern, false);"> 
+	<div id="chooserSpan2" class="dateChooser select-free" style="display: none; visibility: hidden; width: 160px;"></div>
+</div>
+<div id="endtime">
+	End time<select name="endtimehour" id="enttimehour"><?php print_timeselecthour() ?>:<select name="endtimeminute" id = "endtimeminute"><?php print_timeselectminute() ?>
+</div>
+<div>
+<input type="button" value="Search" onclick="updateMap();">
+</div>
+</div>
+<div id="map_canvas" ></div>
+
 </body>
 
 <?php print_footer(); ?>
