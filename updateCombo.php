8fc9e3d32f6ed5397553a0472b9d00d1f1e44e33
Blake A Arnold <baa2121@ottawa.clic.cs.columbia.edu>
2010-11-17 00:52:56 -0500

    fixing lock change

--- updateCombo.php
+++ updateCombo.php
@@ -3,7 +3,6 @@ ini_set('display_errors','On');
 include 'layout.php';
 
 $message = "Error updating.";
-
 function updateCombo($bserial, $combo, $user_login) {
 	require 'dblib/dbinfo.php';
 	// Opens a connection to a MySQL server
@@ -15,6 +14,8 @@ function updateCombo($bserial, $combo, $user_login) {
 	}
 	
 	$query_owner = 'select * from bikes natural join locks natural join bikeowner where login = \'' . $user_login . 	'\' and bserial = ' . $bserial;
+	
+
 	$parse_owner = oci_parse($connection, $query_owner);
 	if(!$parse_owner) { die('invalid query'); }
 	$result_owner = oci_execute($parse_owner, OCI_DEFAULT);
@@ -27,7 +28,7 @@ function updateCombo($bserial, $combo, $user_login) {
 		$message = "You don't own the bike with serial " . $bserial . " or this bike does not exist.";
 	}
 	else {
-		$query_update = 'update locks set combo = \'' . $combo . '\' where lserial = ' . $row_owner[1];
+		$query_update = 'update locks set combo = \'' . $combo . '\' where lserial = ' . $bserial;
 		$parse_update = oci_parse($connection, $query_update);
 		if(!$parse_update) { die('invalid query'); }
 		$result_update = oci_execute($parse_update, OCI_DEFAULT);
@@ -35,13 +36,12 @@ function updateCombo($bserial, $combo, $user_login) {
 			$e = oci_error();
 			trigger_error(htmlentries($e['message'], ENT_QUOTES), E_USER_ERROR);
 		}
-		$message = "Combo updated successfully."
+		$message = "Combo updated successfully.";
 	}
+	oci_commit($connection);
 	oci_close($connection);
 	
 }
-
-echo("hello");
 $bserial = $_POST['bserial'];
 $combo = $_POST['newcombo'];
 updateCombo($bserial, $combo, $_COOKIE['user']);
