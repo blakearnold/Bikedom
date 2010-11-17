4cd4337b5e28819b3d3a9e0713b430957b52a298
Anish Bramhandkar <ab2929@warsaw.clic.cs.columbia.edu>
2010-11-17 15:30:43 -0500

    reservations now print on account dash

--- account.php
+++ account.php
@@ -4,6 +4,7 @@ include 'helper.php';
 include 'dblib/detAction.php';
 include 'dblib/printBikes.php';
 include 'dblib/printUserInfo.php';
+include 'dblib/printReservations.php';
 
 forceLogin();
 print_head_start();
@@ -34,22 +35,28 @@ Bike serial: <input type="text" name="bserial" /> New combo: <input type="text" 
 
 <h4>Your incoming reservations:</h4>
 <div class="accountform">
-<table width=700 align="center">
-<col width=10%>
+<table width=900 align="center">
+<tr><td>Conf Num<td>Deposit<td>Start<td>Stop<td>Owner<td>Renter<td>Serial
+<?php print_res($_COOKIE['user'], 'incoming') ?>
+
+
+<!--<col width=10%>
 <col width=20%>
 <col width=25%>
 <col width=20%>
 <col width=25%>
 <tr><td><h4>Bike ID</hr><td><h4>Reserved by</h4><td><h4>Time period</h4><td><h4>Confirmation #</h4><td><h4>Edit Status</h4>
 <tr><td>1<td>Martha_Kim<td class="pastreserve">2010-01-01 16:00 to<br />2010-01-01 21:00<td>2<td><div class="buttons"><a href="reportdamage.php?bikeId=1&confNum=3" class='button2'>Report Damage</a></div>
-<tr><td>1<td>Martha_Kim<td class="futurereserve">2010-11-25 16:00 to<br />2010-11-25 21:00<td><td><div class="buttons"><a href="cancel.php" class='button2'>Cancel</a></div>
+<tr><td>1<td>Martha_Kim<td class="futurereserve">2010-11-25 16:00 to<br />2010-11-25 21:00<td><td><div class="buttons"><a href="cancel.php" class='button2'>Cancel</a></div>-->
 </table>
 
 </div>
 <h4>Your outgoing reservations:</h4>
 <div class="accountform">
 <table width=900 align="center">
-<col width=10%>
+<tr><td>Conf Num<td>Deposit<td>Start<td>Stop<td>Owner<td>Renter<td>Serial
+<?php print_res($_COOKIE['user'], 'outgoing') ?>
+<!--<col width=10%>
 <col width=20%>
 <col width=25%>
 <col width=15%>
@@ -57,13 +64,13 @@ Bike serial: <input type="text" name="bserial" /> New combo: <input type="text" 
 <col width=15%>
 <tr><td><h4>Bike ID</hr><td><h4>Reserved from</h4><td><h4>Time period</h4><td><h4>Combo</h4><td><h4>Conf Num</h4><td><h4>Cancel?</h4>
 <tr><td>2<td>Paul_Blaer<td class="pastreserve">2010-01-01 16:00 to<br />2010-01-01 21:00<td><td>9<td>
-<tr><td>3<td>Junfeng_Yang<td class="futurereserve">2010-11-27 16:00 to<br />2010-11-27 21:00<td><td>10<td><div class="buttons"><a href="cancel.php" class='button2'>Cancel</a></div>
+<tr><td>3<td>Junfeng_Yang<td class="futurereserve">2010-11-27 16:00 to<br />2010-11-27 21:00<td><td>10<td><div class="buttons"><a href="cancel.php" class='button2'>Cancel</a></div>-->
 </table>
 </div>
 </div>
 
-Test space:
-<?php get_reservation_status(1, $_COOKIE['user']) ?>
+<!--Test space:
+<?php get_reservation_status(1, $_COOKIE['user']) ?>-->
 
 <?php print_footer() ?>
 
---
+++ dblib/printReservations.php
@@ -0,0 +1,50 @@
+<?php
+ini_set('display_errors', 'On'); 
+require 'dbinfo.php';
+
+$is_incoming = 'incoming';
+$is_outgoing = 'outgoing';
+
+function print_res($user_login, $type) {
+require 'dbinfo.php';
+global $is_incoming;
+global $is_outgoing;
+
+// Opens a connection to a MySQL server
+$connection=oci_connect($username, $password, $database);
+if (!$connection) {
+	  $e = oci_error();
+	  trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+}
+
+if(strcmp($type, $is_incoming)==0) {
+	$query_res = 'select * from reservation where ologin=\'' . $user_login . '\'';
+}
+else {
+	$query_res = 'select * from reservation where rlogin=\'' . $user_login . '\'';
+}
+$parse_res = oci_parse($connection, $query_res);
+if(!$parse_res) { die('Invalid query.'); }
+
+$result_res = oci_execute($parse_res, OCI_DEFAULT);
+
+if (!$result_res) {
+	$e = oci_error();
+	trigger_error(htmlentries($e['message'], ENT_QUOTES), E_USER_ERROR);
+}
+
+
+
+while ($row_res = oci_fetch_array($parse_res, OCI_BOTH)) {
+	echo("<tr><td>" . $row_res[0]);
+	echo("<td>" . $row_res[1]);
+	echo("<td>" . $row_res[2]);
+	echo("<td>" . $row_res[3]);
+	echo("<td>" . $row_res[4]);
+	echo("<td>" . $row_res[5]);
+	echo("<td>" . $row_res[6]);
+}
+
+oci_close($connection);
+}
+?>
